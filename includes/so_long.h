1a479b24b522ff090ffd257752c362d66f14c863
krozis <stelie@student.42.fr>
2022-05-10 17:43:37 +0200

    up path to libs

--- includes/so_long.h
+++ includes/so_long.h
@@ -3,17 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stelie <stelie@student.42.fr>              +#+  +:+       +#+        */
+/*   By: krozis <krozis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 14:21:21 by stelie            #+#    #+#             */
-/*   Updated: 2022/04/13 15:58:15 by stelie           ###   ########.fr       */
+/*   Updated: 2022/05/10 17:36:09 by krozis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
-# include "mlx/mlx.h"
-# include "mlx/mlx_int.h"
+# include "libs/mlx/mlx.h"
+# include "libs/mlx_int.h"
+# include "libs/libft/includes/libft.h"
 
 int	so_long(void);
 
