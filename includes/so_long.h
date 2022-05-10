ddbe18573e5fef674853ac5fb41ad9854348f78d
krozis <stelie@student.42.fr>
2022-05-10 17:47:27 +0200

    cp .h to includes

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,15 +6,15 @@
 /*   By: krozis <krozis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 14:21:21 by stelie            #+#    #+#             */
-/*   Updated: 2022/05/10 17:36:09 by krozis           ###   ########.fr       */
+/*   Updated: 2022/05/10 17:44:34 by krozis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
-# include "libs/mlx/mlx.h"
-# include "libs/mlx_int.h"
-# include "libs/libft/includes/libft.h"
+# include "mlx.h"
+# include "mlx_int.h"
+# include "libft.h"
 
 int	so_long(void);
 
