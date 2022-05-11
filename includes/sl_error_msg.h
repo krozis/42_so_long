0d83a2d50566aa94de6bf75d324b5c7acdb5e73f
krozis <stelie@student.42.fr>
2022-05-11 19:36:14 +0200

    updated ERR_EXTENSION

--- includes/sl_error_msg.h
+++ includes/sl_error_msg.h
@@ -6,17 +6,16 @@
 /*   By: krozis <krozis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 16:28:23 by krozis            #+#    #+#             */
-/*   Updated: 2022/05/11 18:35:11 by krozis           ###   ########.fr       */
+/*   Updated: 2022/05/11 19:35:04 by krozis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SL_ERROR_MSG_H
 # define SL_ERROR_MSG_H
-
 /*
 	Errors in map file
 */
-# define ERR_EXTENSION "Map file has a wrong extension.\n"
+# define ERR_EXTENSION "Map file has a wrong extension (*.ber format only).\n"
 # define ERR_ARGS "No Argument or too much argument (just one is required).\n"
 # define ERR_EXISTS "File does not exist.\n"
 # define ERR_MAP_SMALL "Map is too small.\n"
