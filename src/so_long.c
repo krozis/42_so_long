4ff6537c168c8963382d5f2384ce3201378d8f22
krozis <stelie@student.42.fr>
2022-05-11 16:49:56 +0200

    beginning of so_long.c

--- src/so_long.c
+++ src/so_long.c
@@ -3,27 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stelie <stelie@student.42.fr>              +#+  +:+       +#+        */
+/*   By: krozis <krozis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 15:52:16 by stelie            #+#    #+#             */
-/*   Updated: 2022/04/13 15:56:31 by stelie           ###   ########.fr       */
+/*   Updated: 2022/05/11 16:43:23 by krozis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-	#include "so_long.h"
+#include "so_long.h"
 
-int	so_long(void)
+int main(void)
 {
 	void	*mlx_ptr;
+	void	*win_ptr;
 
 	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My first window!");
+	mlx_destroy_window(mlx_ptr, win_ptr);
 	mlx_destroy_display(mlx_ptr);
 	free(mlx_ptr);
-	return (0);
-}
-
-int	main(void)
-{
-	so_long();
-	return (0);
 }
