6ab742a72a988a9b56ce8e98289d48990b3faf88
krozis <stelie@student.42.fr>
2022-05-11 18:40:48 +0200

    updated sl_print_error to return EXIT_SUCCESS version too)

--- src/sl_errors.c
+++ src/sl_errors.c
@@ -6,14 +6,20 @@
 /*   By: krozis <krozis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 18:17:27 by krozis            #+#    #+#             */
-/*   Updated: 2022/05/11 18:27:41 by krozis           ###   ########.fr       */
+/*   Updated: 2022/05/11 18:39:23 by krozis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	sl_print_error(char *error_type)
+int	sl_print_error(char *msg)
 {
-	ft_printf("\033[31m\n\t%s\033[0m\n", error_type);
-	return (1);
+	if (!ft_strncmp(msg, SL_EXIT_SUCCESS,
+			ft_max(ft_strlen(msg), ft_strlen(SL_EXIT_SUCCESS))))
+	{
+		ft_printf("\033[32m\n\t%s\033[0m\n", msg);
+		return (EXIT_SUCCESS);
+	}
+	ft_printf("\033[31m\n\t%s\033[0m\n", msg);
+	return (EXIT_FAILURE);
 }
